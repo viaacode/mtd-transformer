dbbab4001becbbda809cbd1c13a04c68e02e6561
Rudolf <rudolf.degeijter@viaa.be>
2019-12-20 10:23:26 +0100

    AD-373 First version of the CLI. Currently only possible to transform xml with existing XSLT.

---
+++ cli.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+import click
+
+from transformer.transformer import Transformer
+
+@click.group()
+@click.option("--verbose", "-v", is_flag=True)
+def cli(verbose):
+    "Coole CLI voor metadata transformaties"
+    if verbose:
+        click.echo(click.style("VERBOOS", fg="red"))
+    pass
+
+@cli.command(short_help="Transforms an input file to a new format based on the transformation.")
+@click.option("--input", "-i", "input_file", prompt=True, help="File path of the input file.")
+@click.option("--output", "-o", help="Path of the output file.")
+@click.option("--transformation", "-t", prompt=True, help="Transformation to be used.")
+def transform(input_file, output, transformation):
+    extension = Path(input_file).suffix
+    with open(input_file) as file:
+        data = file.read()
+    transformer = Transformer()
+    click.echo(transformer.transform_xml(data, transformation))
+
+
+@cli.command()
+@click.option("--input", "-i", "input_file", prompt=True, help="File path of the input file.")
+@click.option("--validator", "-v", prompt="Location of the XSD-file", help="Path of the output file.")
+def validate(input_file, validator):
+    print(input_file)
+    print(validator)
+
+
+if __name__ == "__main__":
+    cli()
